4a309c12c8fa79eecaacb1e7b82c9c556015517b
zhang <zhang_zhikai@outlook.com>
2025-05-13 18:56:59 +0900

    fix: to run the training for long/short prediction

--- code/Transformer/script/train_mae.sh
+++ code/Transformer/script/train_mae.sh
@@ -28,3 +28,6 @@ python main.py \
       --num_workers 10 \
       --devices 0 \
 
+# --num_workers 10 这里要改成 "0"
+
+# python main.py --project_name transformer_CSI_mae --exp_type mae --data_name CSI --data_type stock --root_path ../../data/ --full_stock_path CSI/ --enc_in 96 --dec_in 96 --c_out 96 --short_term_len 1 --long_term_len 5 --d_model 128 --n_heads 4 --e_layers 2 --d_layers 1 --d_ff 256 --dropout 0.05 --train_epochs 50 --itr 1 --batch_size 32 --learning_rate 0.0001 --adjust_interval 10 --num_workers 10 --devices 0
